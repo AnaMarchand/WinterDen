b26a641d71378a6e6f1fb49c0b0c1e39b6d39476
AnaMarchand <ana.marchand@hotmail.com>
2019-03-13 19:51:59 +0100

    BotonOculto

---
+++ Assets/Pruebas/AnaMarchand/Scripts/ControladorOculto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorOculto : MonoBehaviour
+{
+    public void CargaOculto()
+    {
+        ControladorOculto.LoadScene("NivelOculto");
+    }
+
+    private static void LoadScene(string v)
+    {
+        throw new NotImplementedException();
+    }
+}
